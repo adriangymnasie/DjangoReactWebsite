7abd1b62f93e52d4c93875410d65127e33ab852d
Hibit123 <Philip.lindefors@Outlook.com>
2026-04-23 07:49:51 +0200

    67

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,8 +10,8 @@ function App() {
   const [date, setDate] = useState(new Date());
 
   const images = [
-    'https://picsum.photos/800/400?random=1',
-    'https://picsum.photos/800/400?random=2',
+    "https://d.ibtimes.com/en/full/4631936/67-meme-kid.jpg?w=736&f=7865325e2f7823c1f2a16563bcf0a094",
+    'https://image.zeta-ai.io/profile-image/c06a4529-2bb2-4a05-8fe0-f8379befe252/472a98c3-f13f-429f-8208-8ca78a82ba39/b11f3c7d-c6e1-4069-98af-3ee3a2974cf6.jpeg?w=828&q=90&f=webp',
     'https://picsum.photos/800/400?random=3',
   ];
 
