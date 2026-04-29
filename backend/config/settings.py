b6333c1ae9345b1bda37a95615be900b4485e9fb
Hibit123 <Philip.lindefors@Outlook.com>
2026-04-29 12:13:26 +0200

    pulla

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -53,12 +53,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myapp',
-        'USER': 'dbuser',
-        'PASSWORD': 'dbpassword',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
