68b23d09855bb46279d0da7e3b6afe133daaf2ea
Hibit123 <Philip.lindefors@Outlook.com>
2026-05-08 14:55:23 +0200

    hej

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -75,7 +75,7 @@ TIME_ZONE = 'Europe/Stockholm'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
