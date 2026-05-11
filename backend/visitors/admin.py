b6af5c68e3f81acd881ecdf3320c9f0f82b3a8e7
Hibit123 <Philip.lindefors@Outlook.com>
2026-05-11 09:23:30 +0200

    admind

--- backend/visitors/admin.py
+++ backend/visitors/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Visitor, Todo, Message
 
-# Register your models here.
+admin.site.register(Visitor)
+admin.site.register(Todo)
+admin.site.register(Message)
